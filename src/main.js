573605fb1b69f373b124969a5c5cc50598111c10
Alphie01 <alpsel3@gmail.com>
2021-02-10 23:19:19 +0300

    50% responsive

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import React,{useState,useEffect} from 'react'
 import './App.css';
 import {useParams , Link} from 'react-router-dom';
 import pages from './pages.json';
-import {Container , Row , Col} from 'react-bootstrap'
+import {Container , Row , Col} from 'react-bootstrap';
+import { useMediaQuery } from 'react-responsive';
+import 'react-calendar/dist/Calendar.css';
+import Calendar from 'react-calendar';
 function Main() {
     let { topicId } = useParams();
     const [Pagenum, setPagenum] = useState(0);
@@ -11,6 +14,7 @@ function Main() {
     const [Sponsor, setSponsor] = useState(false)
     const [Partner, setPartner] = useState(false)
     const [Contact, setContact] = useState(false)
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
     useEffect(() => {
         if(topicId==="Python"){
             setPagenum(0);
@@ -25,7 +29,7 @@ function Main() {
     }, [])
     return (
         <div className="Main">
-            <div className="main_screen">
+            <div className={isTabletOrMobile?"mobile_main_screen":"main_screen"}>
                 {
                     Pagenum==-1?(
                         <div className="not_found">
@@ -39,26 +43,24 @@ function Main() {
                                 Main?(
                                     <div className="container mt-5">
                                         <div className="row height_50">
-                                            <div className="col-6">
+                                            <div className="col-lg-6 col-sm-12">
                                                 <h3>
                                                 {
                                                     pages[Pagenum].desc
                                                 }
                                                 </h3>
                                             </div>
-                                            <div className="col-6">
-                                                <h3>
-                                                {
+                                            <div className="col-lg-6 col-sm-12">
+                                                <img src={
                                                     pages[Pagenum].img
-                                                }
-                                                </h3>
+                                                } alt=""/>
                                             </div>
                                         </div>
-                                        <div className="row mt-5">
+                                        <div className="row mt-5 mb-5">
                                             
                                             {
                                                 pages[Pagenum].campInfo.map(adds=>(
-                                                        <div className="col-4 campInfo">
+                                                        <div className="col-lg-4 col-sm-12 campInfo">
                                                             {
                                                                 
                                                                     <div>
@@ -80,6 +82,42 @@ function Main() {
                                                         ))
                                             }
 
+                                        <div className="container">
+                                            <div className="row mt-5 mb-5">
+                                                <div className="col-lg-6 col-sm-12">
+                                                {
+                                                    pages[Pagenum].events.map(adds=>(
+                                                                <div className=" calenders">
+                                                                    <h3>
+                                                                        {adds.speakers}
+                                                                    </h3>
+                                                                    <h4>
+                                                                        {adds.date}
+                                                                    </h4>
+                                                                </div>
+                                                            ))
+                                                }
+                                                </div>
+                                                <div className="col-lg-6 col-sm-12 d-flex justify-content-center align-items-center">
+                                                    {
+                                                        pages[Pagenum].time1?(
+                                                            <Calendar value={[new Date(2017, 1, 27), new Date(2017, 2, 11)]}/>
+                                                        ):(
+                                                            <>
+                                                                {
+                                                                    pages[Pagenum].time2?(
+                                                                        <Calendar value={new Date(2017, 2, 20)}/>
+                                                                    ):(
+                                                                        <Calendar value={[new Date(2017, 2, 23), new Date(2017, 3, 8)]}/>
+                                                                    )
+                                                                }
+                                                            </>
+                                                        )
+                                                    }
+                                                </div>
+
+                                            </div>
+                                        </div>
 
                                         </div>
                                     </div>
@@ -92,7 +130,7 @@ function Main() {
                                             
                                                         {
                                                             pages[Pagenum].speaker.map(adds=>(
-                                                                    <div className="col-4 mt-5">
+                                                                    <div className="col-lg-4 col-sm-12 mt-5">
                                                                         {
                                                                             
                                                                                 <div>
@@ -127,7 +165,7 @@ function Main() {
                                                                     {
                                                                         pages[Pagenum].sponsor.map(adds=>(
                                                                                 
-                                                                                    <div className="col-4 mt-5">
+                                                                                    <div className="col-lg-4 col-sm-12 mt-5">
                                                                                         {
                                                                                             <Link>
                                                                                                 <div>
@@ -157,7 +195,7 @@ function Main() {
                                                                                 {
                                                                                     pages[Pagenum].partner.map(adds=>(
                                                                                             
-                                                                                                <div className="col-4 mt-5">
+                                                                                                <div className="col-lg-4 col-sm-12 mt-5">
                                                                                                     {
                                                                                                         <Link>
                                                                                                             <div>
@@ -204,8 +242,8 @@ function Main() {
                     )
                 }
             </div>
-            <div className="nav">
-                <div className="navlink" onClick={(e)=>{
+            <div className={isTabletOrMobile?"display_none":"nav"}>
+                <div className={Main?"navlink active":"navlink"} onClick={(e)=>{
                     e.preventDefault()
                     setPartner(false)
                     setSponsor(false)
@@ -216,7 +254,7 @@ function Main() {
                     <div className="dot"></div>
                     <h1>Kamp detayları</h1>
                 </div>
-                <div className="navlink" onClick={(e)=>{
+                <div className={Konuşmacı?"navlink active":"navlink"} onClick={(e)=>{
                     e.preventDefault()
                     setPartner(false)
                     setSponsor(false)
@@ -227,7 +265,7 @@ function Main() {
                     <div className="dot"></div>
                     <h1>Kamp Konuşmacılar</h1>
                 </div>
-                <div className="navlink" onClick={(e)=>{
+                <div className={Sponsor?"navlink active":"navlink"} onClick={(e)=>{
                     e.preventDefault()
                     setPartner(false)
                     setSponsor(true)
@@ -238,7 +276,7 @@ function Main() {
                     <div className="dot"></div>
                     <h1>Kamp Sponsorları</h1>
                 </div>
-                <div className="navlink" onClick={(e)=>{
+                <div className={Partner?"navlink active":"navlink"} onClick={(e)=>{
                     e.preventDefault()
                     setPartner(true)
                     setSponsor(false)
@@ -249,7 +287,7 @@ function Main() {
                     <div className="dot"></div>
                     <h1>Kamp Partnerlerı</h1>
                 </div>
-                <div className="navlink" onClick={(e)=>{
+                <div className={Contact?"navlink active":"navlink"} onClick={(e)=>{
                     e.preventDefault()
                     setPartner(false)
                     setSponsor(false)
